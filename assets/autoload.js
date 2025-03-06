28df60f476b5944a61ba46d682f71ab950aa14f8
FuyukiVila <1642421711@qq.com>
2025-03-06 16:39:00 +0800

    refactor/assets: 移除 autoload.js 中的版本号

--- assets/autoload.js
+++ assets/autoload.js
@@ -1,9 +1,9 @@
 try {
     const path = 'https://cdn.jsdelivr.net/gh/fuyukivila/live2d_demo@master/'
-    $("<link>").attr({href: path + "assets/waifu.min.css?v=1.4.2", rel: "stylesheet", type: "text/css"}).appendTo('head');
+    $("<link>").attr({href: path + "assets/waifu.min.css", rel: "stylesheet", type: "text/css"}).appendTo('head');
     $('body').append('<div class="waifu"><div class="waifu-tips"></div><canvas id="live2d" class="live2d"></canvas><div class="waifu-tool"><span class="fui-home"></span> <span class="fui-chat"></span> <span class="fui-eye"></span> <span class="fui-user"></span> <span class="fui-photo"></span> <span class="fui-info-circle"></span> <span class="fui-cross"></span></div></div>');
-    $.ajax({url: path + "assets/waifu-tips.min.js?v=1.4.2", dataType:"script", cache: true, success: function() {
-            $.ajax({url: path + "assets/live2d.min.js?v=1.0.5", dataType:"script", cache: true, success: function() {
+    $.ajax({url: path + "assets/waifu-tips.min.js", dataType:"script", cache: true, success: function() {
+            $.ajax({url: path + "assets/live2d.min.js", dataType:"script", cache: true, success: function() {
                     /* 可直接修改部分参数 */
                     live2d_settings['hitokotoAPI'] = "hitokoto.cn";  // 一言 API
                     live2d_settings['modelId'] = 1;                  // 默认模型 ID
